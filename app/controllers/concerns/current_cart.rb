3328779517e1a6405aa4fe3f311c68f3a1989224
CJ <cjzhang88@gmail.com>
2018-04-28 01:55:21 +0800

    添加按钮

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -3,8 +3,8 @@ module CurrentCart
   
   def set_cart
     @cart = Cart.find(session[:cart_id])
-  rescue ActiveRecord:RecordNotFound
-    @cart = Cart.new
+  rescue ActiveRecord::RecordNotFound
+    @cart = Cart.create
     session[:cart_id] = @cart.id
   end
   
