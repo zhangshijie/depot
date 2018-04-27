8386836e417b38965e2796c8f897943d5b7f7125
CJ <cjzhang88@gmail.com>
2018-04-28 00:43:30 +0800

    查找购物车

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_25_051440) do
+ActiveRecord::Schema.define(version: 2018_04_27_163744) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
